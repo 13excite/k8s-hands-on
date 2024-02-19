91ba85e5f35350dded23fb339127abb4b01300e2
Vladimir Dulenov <vladimir.dulenov@vent.io>
2024-02-19 10:38:37 +0100

    feat: setup.sh was improved

--- setup.sh
+++ setup.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 KIND_CLUSTER_NAME="test-cluster"
+NO_COLOR='\033[0m'
+OK_COLOR='\033[32;01m'
+ERR_COLOR='\033[0;31m'
 
 # check that kind is installed
 if ! command -v kind &> /dev/null
 then
-    echo "kind binary could not be found"
+    printf "${ERR_COLOR}kind binary could not be found ${NO_COLOR}\n"
     exit 1
 fi
 
 # check that kubectl is installed
 if ! command -v kubectl &> /dev/null
 then
-    echo "kubectl binary could not be found"
+    printf "${ERR_COLOR}kubectl binary could not be found${NO_COLOR}\n"
     exit 1
 fi
 
-kind get clusters|grep  $KIND_CLUSTER_NAME &>/dev/null && echo "Cluster $KIND_CLUSTER_NAME already exists...." && exit 1
+kind get clusters|grep  $KIND_CLUSTER_NAME &>/dev/null && printf "${ERR_COLOR}Cluster with name: ${KIND_CLUSTER_NAME} already exists....${NO_COLOR}\n" && exit 1
 
 
 kind create cluster --name $KIND_CLUSTER_NAME --config ./kind/kind.yaml
@@ -27,9 +30,9 @@ sleep 15
 
 # check that k8s context is correctly set
 if kubectl config get-contexts |tail -n +2|awk {'print $2'}|grep -q "kind-${KIND_CLUSTER_NAME}"; then
-    echo "Cluster name ${KIND_CLUSTER_NAME} found and k8s context is correctly set. Continuing..."
+    printf "${OK_COLOR}Cluster with name: ${KIND_CLUSTER_NAME} found and k8s context is correctly set. Continuing...${NO_COLOR}\n"
 else
-    echo "Cluster name ${KIND_CLUSTER_NAME} not found or k8s context is incorrect. Exiting..."
+    printf "${ERR_COLOR}Cluster with name: ${KIND_CLUSTER_NAME} not found or k8s context is incorrect. Exiting...${NO_COLOR}\n"
     exit 1
 fi
 
@@ -48,11 +51,12 @@ kubectl label nodes test-cluster-worker gputype=rtx4060
 kubectl apply -f ./spec/namespaces.yaml
 
 # create tasks
-kubectl apply -f ./spec/task1/ 
+kubectl apply -f ./spec/task1/
 sleep 10
 
 kubectl apply -f ./spec/task2/
 kubectl apply -f ./spec/task3/
 kubectl apply -f ./spec/task4/
-kubectl apply -f spec/99_deploy4.yaml
+kubectl apply -f ./spec/99_deploy4.yaml
+kubectl apply -f ./spec/task5/
 
