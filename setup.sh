df669e8b4bbcae0893f9cb3f4aa69ea33a3e8dcd
Vladimir Dulenov <v.dulenov@link11.com>
2026-08-16 18:01:35 +0200

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -59,7 +59,7 @@ kubectl apply -f ./envoy-gw/ref-grants.yaml
 printf "Waiting for the envoy proxy service for gateway main-gw...\n"
 GW_SVC=""
 for i in $(seq 1 30); do
-    GW_SVC=$(kubectl get svc -n basic-gw \
+    GW_SVC=$(kubectl get svc -n envoy-gateway-system \
       -l gateway.envoyproxy.io/owning-gateway-namespace=basic-gw,gateway.envoyproxy.io/owning-gateway-name=main-gw \
       -o jsonpath='{.items[0].metadata.name}' 2>/dev/null)
     [ -n "$GW_SVC" ] && break
@@ -72,7 +72,7 @@ if [ -z "$GW_SVC" ]; then
 fi
 
 # pin the NodePort to 30080 so it matches kind's hostPort 80 -> containerPort 30080 mapping
-kubectl patch svc "$GW_SVC" -n basic-gw --type=json \
+kubectl patch svc "$GW_SVC" -n envoy-gateway-system --type=json \
   -p '[{"op": "replace", "path": "/spec/ports/0/nodePort", "value": 30080}]'
 
 # create tasks
